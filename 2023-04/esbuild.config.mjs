2d21ed9ec99f68414d0dad86651394420474fae4
gct256 <gct256+github@gmail.com>
2023-03-30 17:19:37 +0900

    2023-04版作成

---
+++ 2023-04/esbuild.config.mjs
@@ -0,0 +1,24 @@
+import esbuildServe from "esbuild-serve";
+
+const dist = !process.argv.includes('-w');
+
+esbuildServe(
+  {
+    entryPoints: ["./src/script.tsx"],
+    bundle: true,
+    outfile: "./app/script.js",
+    platform: "browser",
+    target: "esnext",
+    define: {
+      "process.env.NODE_ENV": JSON.stringify(
+        dist ? 'production' : "development",
+      ),
+    },
+    minify: dist,
+    sourcemap: true,
+  },
+  {
+    port: 3030,
+    root: "./app",
+  },
+);
